48bcf66c1585ea38737a3194ec8f0476b438135e
mottoslo <57926597+mottoslo@users.noreply.github.com>
2021-05-16 04:48:23 +0900

    Add files via upload

---
+++ 4.ResNet/resnet_infer.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+import data_helpers as dh
+import numpy as np
+
+# Eval Parameters
+tf.flags.DEFINE_string("checkpoint_dir", "./runs/setting2/checkpoints", "Checkpoint directory from training run")
+
+FLAGS = tf.flags.FLAGS
+
+dataset = dh.read_my_images("C:\\Users\\boss\\Desktop\\example\\*")
+
+checkpoint_file = tf.train.latest_checkpoint(FLAGS.checkpoint_dir)
+graph = tf.Graph()
+with graph.as_default():
+    sess = tf.Session()
+    with sess.as_default():
+        saver = tf.train.import_meta_graph("{}.meta".format(checkpoint_file))
+        saver.restore(sess, checkpoint_file)
+
+        # Get the placeholders from the graph by name
+        X = graph.get_operation_by_name("X").outputs[0]
+        Y = graph.get_operation_by_name("Y").outputs[0]
+        predictions = graph.get_operation_by_name("logit/predictions").outputs[0]
+
+        inference_result = sess.run(predictions, feed_dict={X: dataset[0]})
+        for i in dataset[1]:
+            print(i)
+        print("airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck")
+        print(inference_result)
---
+++ 4.ResNet/test.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import os
+import time
+import datetime
+from tensorflow.keras.datasets.cifar10 import load_data
+import data_helpers as dh
+from resnet import ResNet
+
+print(load_data)
